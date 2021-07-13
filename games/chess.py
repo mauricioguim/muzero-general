9983f9fecd50529ae3c8f472680c24095f315b3b
Maurício <mauricioguim@users.noreply.github.com>
2021-07-13 09:29:32 -0300

    Update chess.py

--- games/chess.py
+++ games/chess.py
@@ -21,7 +21,7 @@ class MuZeroConfig:
 
 
         ### Game
-        self.observation_shape = (8,8,8)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
+        self.observation_shape = (3,8,8)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
         self.action_space = list(range(4672))  # Fixed list of all possible actions. You should only edit the length
         self.players = list(range(2))  # List of players. You should only edit the length
         self.stacked_observations = 0  # Number of previous observations and previous actions to add to the current observation
